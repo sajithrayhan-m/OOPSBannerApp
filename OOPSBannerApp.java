559444f022e38e8fd429ed66c99f9e3f0afad3d7
Sajith Rayhan~ <sr2060@srmist.edu.in>
2026-03-10 09:24:12 +0530

    UC7: Implemented CharacterPatternMap inner class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,54 +1,112 @@
 public class OOPSBannerApp {
 
+    /* Inner class to store character and its pattern */
+    static class CharacterPatternMap {
+
+        private char character;
+        private String[] pattern;
+
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    /* Create patterns for characters */
+    public static CharacterPatternMap[] createCharacterPatternMaps() {
+
+        String[] oPattern = {
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+
+        String[] pPattern = {
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+
+        String[] sPattern = {
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+
+        String[] spacePattern = {
+            "     ",
+            "     ",
+            "     ",
+            "     ",
+            "     ",
+            "     ",
+            "     "
+        };
+
+        return new CharacterPatternMap[]{
+            new CharacterPatternMap('O', oPattern),
+            new CharacterPatternMap('P', pPattern),
+            new CharacterPatternMap('S', sPattern),
+            new CharacterPatternMap(' ', spacePattern)
+        };
+    }
+
+    /* Find pattern for a character */
+    public static String[] getCharacterPattern(char ch, CharacterPatternMap[] maps) {
+
+        for (CharacterPatternMap map : maps) {
+            if (map.getCharacter() == ch) {
+                return map.getPattern();
+            }
+        }
+
+        return maps[3].getPattern(); // return space pattern
+    }
+
+    /* Print banner message */
+    public static void printMessage(String message, CharacterPatternMap[] maps) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : message.toCharArray()) {
+
+                String[] pattern = getCharacterPattern(ch, maps);
+
+                line.append(pattern[row]).append(" ");
+            }
+
+            System.out.println(line);
+        }
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(String.join("  ",
-                " ***** ",
-                " ***** ",
-                " ******",
-                " ***** "
-        ));
+        CharacterPatternMap[] maps = createCharacterPatternMaps();
 
-        System.out.println(String.join("  ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*      "
-        ));
+        String message = "OOPS";
 
-        System.out.println(String.join("  ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*      "
-        ));
-
-        System.out.println(String.join("  ",
-                "*     *",
-                "*     *",
-                "****** ",
-                " ***** "
-        ));
-
-        System.out.println(String.join("  ",
-                "*     *",
-                "*     *",
-                "*      ",
-                "      *"
-        ));
-
-        System.out.println(String.join("  ",
-                "*     *",
-                "*     *",
-                "*      ",
-                "*     *"
-        ));
-
-        System.out.println(String.join("  ",
-                " ***** ",
-                " ***** ",
-                "*      ",
-                " ***** "
-        ));
+        printMessage(message, maps);
     }
 }
